00037ffab43ff12d7eff2e0c2175a070feddce25
skea <253590433@qq.com>
2021-12-22 00:00:31 +0800

    useEffect更新机制

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { hot } from 'react-hot-loader/root'
 import Test from '../testComponent/testCmp'
 
 function App (props: any) {
   const [count, setCount] = useState(0)
-  const { name } = props
-  const handleClick = () => {
-    setCount(count + 1)
-  }
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(count => count + 1)
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [])
   return (
-    <div
-      onClick={handleClick}
-      onKeyDown={handleClick}
-      role="button"
-      tabIndex={0}
-    >
-      {name}124
-      {count}
+    <div>
+      <h1>{count}</h1>
       <Test />
     </div>
   )
