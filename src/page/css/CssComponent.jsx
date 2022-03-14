f5f6e55ace3b2794f11023a8233a5d05786c3ea4
mianmalife <253590433@qq.com>
2022-03-14 23:34:39 +0800

    css lulu ui button start

--- src/page/css/CssComponent.jsx
+++ src/page/css/CssComponent.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import 'lu2/theme/edge/css/common/ui/Button.css'
+import './index.css'
 function CssComponent () {
-  return <div>css</div>
+  return <div id='csscomponent__wrapper'>
+    <div className='lulu__ui__wrapper'>
+    <h2>Lu Lu UI edge主题</h2>
+    <h3>Lu Lu UI button</h3>
+    <p><button className='ui-button'>lulu ui button</button></p>
+    <p><button className='ui-button loading'>lulu ui loading</button></p>
+    <p><button className='ui-button disabled'>lulu ui disabled</button></p>
+    <p><button is='ui-button'>is属性 lulu ui disabled</button></p>
+    <p><button is='ui-button' type='primary'>is属性 lulu ui primary</button></p>
+    </div>
+  </div>
 }
 
 export default CssComponent
