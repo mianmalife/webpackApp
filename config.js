64194451b8e388a8a17d4630f1fbb4c295c8580d
mianmalife <253590433@qq.com>
2022-04-11 15:55:55 +0000

    Deploying to gh-pages from @ mianmalife/webpack5-app@5c85a64b446c8c3f3a2aa36558cd90531f489023 🚀

---
+++ config.js
@@ -0,0 +1,4 @@
+const COMMON_URL = 'https://www.fastmock.site'
+const COMMON_URL1="https://www.fastmock.site"
+const COMMON_URL2="https://www.fastmock.site"
+const COMMON_URL3="https://www.fastmock.site"
