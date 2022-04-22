56091e365ef3fafae0e45de1f6fbac273abca23b
mianmalife <253590433@qq.com>
2022-04-22 13:15:22 +0000

    Deploying to gh-pages from @ mianmalife/webpack5-app@d128acb5747d03ff6afec54a273814cb5164ea37 🚀

---
+++ config.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://www.fastmock.site'
+export const baseUrl1="https://www.fastmock.site"
+export const baseUrl2="https://www.fastmock.site"
+export const baseUrl3="https://www.fastmock.site"
